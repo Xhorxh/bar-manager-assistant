4fd8f3803270565fc0d17e63ac467293d3c8d60e
George <andrea.xhorxhi@gmail.com>
2022-07-12 12:07:37 +0000

    Add validation to check for exactly 8 values in provided data

--- run.py
+++ run.py
@@ -22,7 +22,24 @@ def input_sales_data():
     print("Example: 10,20,30,40,50,60,70,80\n")
 
     data_str = input("Enter your data here: ")
-    print(f"The data provided is {data_str}")
+
+    sales_data = data_str.split(',')
+    validate_data(sales_data)
+
+
+def validate_data(values):
+    """
+    Inside the try, converts all string values into integers.
+    Raises ValueError if strings cannot be converted into int,
+    or if there aren't exactly 8 values.
+    """
+    try:
+        if len(values) != 8:
+            raise ValueError(
+                f"Exactly 8 values required, you provided {len(values)}"
+            )
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again.\n")
 
 
 input_sales_data()
